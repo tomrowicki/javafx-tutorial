e0b06f7d3629db19891d34ea14345badd73ba65c
Tomasz Mrowicki <tomrowicki@gmail.com>
2023-11-05 15:13:27 +0100

    Events - continued

--- src/main/java/tomrowicki/javafx/EventsController.java
+++ src/main/java/tomrowicki/javafx/EventsController.java
@@ -3,6 +3,7 @@ package tomrowicki.javafx;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 
 public class EventsController {
@@ -12,6 +13,8 @@ public class EventsController {
     private Button helloButton;
     @FXML
     private Button byeButton;
+    @FXML
+    private CheckBox ourCheckbox;
 
     // this requires the exact name 'initialize' to work outside the box
     @FXML
@@ -21,15 +24,20 @@ public class EventsController {
     }
 
     // can use this without specifying event; can make the method private but add annotation
-    public void onButtonClicked() {
-        System.out.printf("Hello, %s!%n", nameField.getText());
-    }
+//    public void onButtonClicked() {
+//        System.out.printf("Hello, %s!%n", nameField.getText());
+//    }
 
     public void onEitherButtonClicked(ActionEvent e) {
         if (e.getSource().equals(helloButton)) {
             System.out.printf("Hello, %s!%n", nameField.getText());
         } else if (e.getSource().equals(byeButton)) {
             System.out.printf("Bye, %s!%n", nameField.getText());
+        }
+        if (ourCheckbox.isSelected()) {
+            nameField.clear();
+            helloButton.setDisable(true);
+            byeButton.setDisable(true);
         }
     }
 
@@ -39,5 +47,9 @@ public class EventsController {
         boolean disabledButtons = text.trim().isEmpty();
         helloButton.setDisable(disabledButtons);
         byeButton.setDisable(disabledButtons);
+    }
+
+    public void handleChange() {
+        System.out.println("The checkbox is " + (ourCheckbox.isSelected() ? "checked" : "unchecked"));
     }
 }

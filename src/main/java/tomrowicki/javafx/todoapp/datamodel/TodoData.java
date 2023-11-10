aa68a7ee50bcc174a55f28eb3d2980c025ac0ff7
Tomasz Mrowicki <tomrowicki@gmail.com>
2023-11-10 13:36:42 +0100

    TODO app - added data storage singleton

---
+++ src/main/java/tomrowicki/javafx/todoapp/datamodel/TodoData.java
@@ -0,0 +1,76 @@
+package tomrowicki.javafx.todoapp.datamodel;
+
+import javafx.collections.FXCollections;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Iterator;
+import java.util.List;
+
+public class TodoData {
+    private static TodoData instance = new TodoData();
+    private static String filename = "TodoListItems.txt";
+
+    private List<TodoItem> todoItems;
+    private DateTimeFormatter formatter;
+
+    private TodoData() {
+        formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    }
+
+    public static TodoData getInstance() {
+        return instance;
+    }
+
+    public void setTodoItems(List<TodoItem> todoItems) {
+        this.todoItems = todoItems;
+    }
+
+    public List<TodoItem> getTodoItems() {
+        return todoItems;
+    }
+
+    public void loadTodoItems() throws IOException {
+        todoItems = FXCollections.observableArrayList();
+        Path path = Paths.get(filename);
+        BufferedReader reader = Files.newBufferedReader(path);
+
+        String input;
+        try (reader) {
+            while ((input = reader.readLine()) != null) {
+                String[] itemPieces = input.split("\t");
+
+                String shortDescription = itemPieces[0];
+                String details = itemPieces[1];
+                String dateString = itemPieces[2];
+                LocalDate date = LocalDate.parse(dateString, formatter);
+                TodoItem todoItem = new TodoItem(shortDescription, details, date);
+                todoItems.add(todoItem);
+
+            }
+        }
+    }
+
+    public void storeTodoItems() throws IOException {
+        Path path = Paths.get(filename);
+        BufferedWriter writer = Files.newBufferedWriter(path);
+
+        try (writer) {
+            Iterator<TodoItem> iter = todoItems.iterator();
+            while (iter.hasNext()) {
+                TodoItem item = iter.next();
+                writer.write(String.format("%s\t%s\t%s",
+                        item.getShortDescription(),
+                        item.getDetails(),
+                        item.getDeadline().format(formatter)));
+                writer.newLine();
+            }
+        }
+    }
+}
